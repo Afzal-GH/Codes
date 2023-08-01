d600932ea71a50d4ee397b218ee029cddbd488fe
SYED AFZAL ALI <75764094+Afzal-GH@users.noreply.github.com>
2023-08-01 05:42:16 +0530

    Update and rename Plot.R to Plot_5.R

--- Plot.R
+++
@@ -1,7 +0,0 @@
-data <- c(0, 2, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0,
-          0, 0, 2, 0, 1, 0, 0, 0, 4, 1, 0, 0, 1, 0, 0, 0, 1, 0)
-
-relative_freq <- table(data) / length(data)
-
-barplot(relative_freq, main = "Relative Frequency Bar Plot", xlab = "Number of Times Taken on Sunday",
-        ylab = "Relative Frequency", col = "steelblue")
